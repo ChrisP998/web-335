5f9253def506f42b37650832828ca9d805c20283
Christopher Phan <giritina444@gmail.com>
2026-01-25 23:35:46 -0500

    new file:   week-2/assignment1.py

---
+++ week-2/assignment1.py
@@ -0,0 +1,8 @@
+"""
+Author: Christopher Phan
+Date: 1/25/26
+File Name: assignment1.py
+Description: This file prints a quote
+"""
+print("In three words I can sum up everything I've learned about life: it goes on.")
+print("-Robert Frost")
